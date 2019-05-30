70f40c6f38eff4a0da4920f3d4fb24268371bf50
etranchi <etranchi@student.42.fr>
2019-05-31 00:29:45 +0200

    neg, need remove meaning less op

--- expert.py
+++ expert.py
@@ -42,7 +42,22 @@ class File:
         self.maxLvl = 0
         self.minLvl = -1
 
+    def displayRules(self):
+        print("Rules :")
+        for r in self.rules:
+            if len(r.hash) > 0:
+                print(r.hash + " : " + str(r.solved))
+    
+    def displayOp(self):
+        print("Operations:")
+        for o in self.operations:
+            if type(o) is Operation and len(o.hash) > 0:
+                print(o.hash + " : " + str(o.solved) + str(o.result))
+            if type(o) is Fact and len(o.name) > 0:
+                print(o.name + " : " + str(o.solved)  + str(o.value))
+
     def displayFacts(self):
+        print("Facts:")
         for f in self.facts:
             if len(f.name) > 0:
                 print(f.name + " : " + str(f.value))
@@ -63,7 +78,23 @@ class File:
             if type(_) is Operation and fact.name == _.hash: 
                 self.operations[i] = fact
                 print("fact added" + fact.name)
+    
+    def set_value_to_op_and_fact(self,lhs, rhs):
+        rhs.solved = 1
+        if type(rhs) is Fact:
+            fact = self.find_fact_same_same(rhs.name, None)
+            if fact:
+                if type(lhs) is Fact:
+                    fact.value = lhs.value
+                    print(fact.name + "set to " + str(lhs.value))
+                else :
+                    fact.value = lhs.result
+                    print(fact.name + "set to " + str(lhs.result))
+                fact.solved = 1
                 
+        else :
+            self.set_value_to_op_and_fact(lhs, rhs.rhs)
+            self.set_value_to_op_and_fact(lhs, rhs.lhs)
         
 
 class Fact:
@@ -75,7 +106,7 @@ class Fact:
         
     def get_v(self):
         if self.is_not:
-            return not self.value
+            return int(not self.value)
         return self.value
 
 
@@ -91,6 +122,7 @@ class Operation:
         self.lhs = None
         self.result = None
         self.solved = 0
+        self.removed = 0
 
     
     def get_op_value(self, str):
@@ -110,7 +142,10 @@ class Operation:
                 operation.parse_each_side()
             return operation
         else:
-            is_not = not (len(str) == 1)
+            is_not = 0
+            if len(str) > 1:
+                is_not = 1
+                str = str[1]
             fact = current_file.find_fact_same_same(str, is_not)
             if not fact:
                 fact = Fact(str , is_not)
@@ -120,6 +155,30 @@ class Operation:
     def parse_each_side(self):
         self.lhs = self.create_operations(self.lhs_s, L_S)
         self.rhs = self.create_operations(self.rhs_s, R_S)
+    
+    def removeMe(self):
+        if not self.removed:
+            self.removeFromOpMe()
+            self.removeFromRulesMe()
+            self.removed = 1
+
+    def removeFromOpMe(self):
+        try:
+            index = current_file.operations.index(self)
+            if index >= 0:
+                current_file.operations.pop(index)
+        except ValueError:
+            return
+        
+    
+    def removeFromRulesMe(self):
+        try:
+            index = current_file.rules.index(self)
+            if index >= 0:
+                current_file.rules.pop(index)
+        except ValueError:
+            return
+
     
     def make_my_operation(self):
         if self.op == "+":
@@ -134,17 +193,11 @@ class Operation:
             name = "IAOI"
         method = getattr(self, name, lambda: "Error")
         self.result = method()
-        self.solved = 1
-        
-        current_file.replace_op_by_fact(self)
+        self.solved = 1 
+        if (name != "IMP" and name != "IAOI"):
+            current_file.replace_op_by_fact(self)
 
     def ADD(self):
-        if self.lhs.get_v() is None and self.rhs.get_v():
-            return self.rhs.get_v()
-        if self.rhs.get_v() is None and self.lhs.get_v():
-            return self.lhs.get_v()
-        if self.rhs.get_v() is None and self.lhs.get_v() is None:
-            return None
         return self.lhs.get_v() and self.rhs.get_v()
 
     def OR(self):
@@ -166,16 +219,28 @@ class Operation:
         return self.lhs.get_v() ^ self.rhs.get_v()
 
     def IMP(self):
-        fact = current_file.find_fact_same_same(self.rhs.name, self.rhs.is_not)
-        if fact:
-            if type(self.lhs) is Fact: 
-                fact.value = self.lhs.value
-            else :
-                fact.value = self.lhs.result
-            print(fact.name + " set to : " + str(fact.value))
-            fact.solved = 1
         self.solved = 1
-        return fact.value
+        if type(self.rhs) is Fact:
+            fact = current_file.find_fact_same_same(self.rhs.name, self.rhs.is_not)
+            if fact:
+                if type(self.lhs) is Fact: 
+                    fact.value = self.lhs.get_v()
+                    print(fact.name + " v set to : " + str(fact.value))
+                else :
+                    if not self.lhs.solved:
+                        self.lhs.make_my_operation()
+                    fact.value = self.lhs.result
+                fact.solved = 1
+                return fact.get_v()
+        else :
+            print("JE PASSE")
+            current_file.set_value_to_op_and_fact(self.lhs, self.rhs)
+            if type(self.lhs) is Fact:
+                return self.lhs.value
+            else :
+                return self.lhs.result
+        
+        
 
     def IAOI(self):
         return self.rhs.get_v() and self.lhs.get_v()
@@ -187,21 +252,30 @@ def make_easy_op():
     easy_op = []
     for op in current_file.operations:
         if type(op) is Operation:
-            if (type(op.lhs) is Fact or type(op.rhs) is Fact) and (op.lhs.solved == 1 or op.rhs.solved == 1):
-                if op.op == "+" or op.op == "|":
-                    op.make_my_operation()
-                    print("Operation done : " + op.hash + "=" + str(op.result))
-            if type(op.lhs) is Operation:
-                print("je suis la")
+            # print("j'ai une op : "+ op.hash)
+            # print(type(op.lhs), type(op.rhs))
+            # print(op.lhs.solved, op.rhs.solved)
+            if (type(op.lhs) is Fact) and (op.lhs.solved == 1 or op.rhs.solved == 1):
+                print("lhs is fact !")
+                op.make_my_operation()
+                op.removeMe()
+                print("Operation done op: " + op.hash + "=" + str(op.result))
+                current_file.displayFacts()
+                return
 def make_easy_rule():
     i = -1
+    
     for op in current_file.rules:
         if op.solved == 0:
+            print("jai une op")
             print(op.hash)
-            print(op.lhs.solved)
-            if op.lhs.solved == 1:
+            if op.lhs.solved == 1 or len(current_file.rules) == 1:
                 op.make_my_operation()
-                print("Operation done : " + op.hash + "=" + str(op.result))
+                print("Operation done ru: " + op.hash + "=" + str(op.result))
+                op.removeMe()
+                current_file.displayFacts()
+                make_easy_rule()
+                return
 
 
 def check_if_solved():
@@ -210,11 +284,15 @@ def check_if_solved():
             return False
     return True
 
-def makeMagic():
+def makeMagic(rec):
     make_easy_op()
     make_easy_rule()
-    if not check_if_solved():
-        makeMagic()
+    if not check_if_solved() and rec < 10:
+        makeMagic(rec + 1)
+    else:
+        current_file.displayFacts()
+        current_file.displayOp()
+        current_file.displayRules()
 
 def set_rule(line):
     operand = None
@@ -261,7 +339,7 @@ def check_file(s_file):
         for line in file:
             check_line(line)
         print("I got everything needed.")
-        makeMagic()
+        makeMagic(0)
         #current_file.orderOperationInRules()
         
         # current_file.makeItSolvable(current_file.maxLvl)
